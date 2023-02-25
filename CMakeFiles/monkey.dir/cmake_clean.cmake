12af7692147b4d4366bb95885a701a8b910f521b
Rickie Blaese <rkyleblaese@gmail.com>
2023-02-24 21:53:01 -0800

    cmake now

---
+++ CMakeFiles/monkey.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monkey.dir/src/monkey.cc.o"
+  "CMakeFiles/monkey.dir/src/monkey.cc.o.d"
+  "monkey"
+  "monkey.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monkey.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
